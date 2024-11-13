b790a3b08ea856619cd2f8c1970c158245799614
glad.e <gladysann1307@gmail.com>
2024-11-13 07:46:40 +0530

    Added list of models

---
+++ src/model_handler.py
@@ -0,0 +1,4 @@
+from modellist import model_list
+
+def list_models():
+    return model_list
---
+++ src/modellist.py
@@ -0,0 +1,227 @@
+model_list = [
+    {
+        "script_name": "Devanagari",
+        "model_name": "Devanagari_model",
+        "downloaded": True,
+        "model_type": "Multinomial Naive Bayes",
+        "vectorizer_model_name": "Devanagari_vectorizer",
+        "vectorizer_type": "CountVectorizer",
+        "vectorizer_params": {
+            "ngram_range": "(3, 3)",
+            "max_features": 2000,
+            "analyzer": "char"
+        },
+        "vectorizer_downloaded": True,
+        "accuracy": "99.05%",
+    },
+    {
+        "script_name": "Arabic",
+        "model_name": "Arabic_model",
+        "downloaded": False,
+        "model_type": "Multinomial Naive Bayes",
+        "vectorizer_model_name": "Arabic_vectorizer",
+        "vectorizer_type": "CountVectorizer",
+        "vectorizer_params": {
+            "ngram_range": "(3, 3)",
+            "max_features": 2000,
+            "analyzer": "char"
+        },
+        "vectorizer_downloaded": False,
+        "accuracy": "99.79%",
+    },
+    {
+        "script_name": "Bengali",
+        "model_name": "Bengali_model",
+        "downloaded": False,
+        "model_type": "Multinomial Naive Bayes",
+        "vectorizer_model_name": "Bengali_vectorizer",
+        "vectorizer_type": "CountVectorizer",
+        "vectorizer_params": {
+            "ngram_range": "(3, 3)",
+            "max_features": 2000,
+            "analyzer": "char"
+        },
+        "vectorizer_downloaded": False,
+        "accuracy": "99.81%",
+    },
+    {
+        "script_name": "Cyrillic",
+        "model_name": "Cyrillic_model",
+        "downloaded": False,
+        "model_type": "Multinomial Naive Bayes",
+        "vectorizer_model_name": "Cyrillic_vectorizer",
+        "vectorizer_type": "CountVectorizer",
+        "vectorizer_params": {
+            "ngram_range": "(3, 3)",
+            "max_features": 2000,
+            "analyzer": "char"
+        },
+        "vectorizer_downloaded": False,
+        "accuracy": "99.67%",
+    },
+    {
+        "script_name": "Ethiopic",
+        "model_name": "Ethiopic_model",
+        "downloaded": False,
+        "model_type": "Multinomial Naive Bayes",
+        "vectorizer_model_name": "Ethiopic_vectorizer",
+        "vectorizer_type": "CountVectorizer",
+        "vectorizer_params": {
+            "ngram_range": "(2, 3)",
+            "max_features": 2000,
+            "analyzer": "char"
+        },
+        "vectorizer_downloaded": False,
+        "accuracy": "98.33%",
+    },
+    {
+        "script_name": "Gujarati",
+        "model_name": "Gujarati_model",
+        "downloaded": False,
+        "model_type": "Multinomial Naive Bayes",
+        "vectorizer_model_name": "Gujarati_vectorizer",
+        "vectorizer_type": "CountVectorizer",
+        "vectorizer_params": {
+            "ngram_range": "(3, 3)",
+            "max_features": 2000,
+            "analyzer": "char"
+        },
+        "vectorizer_downloaded": False,
+        "accuracy": "99.89%",
+    },
+    {
+        "script_name": "Gurmukhi",
+        "model_name": "Gurmukhi_model",
+        "downloaded": False,
+        "model_type": "Multinomial Naive Bayes",
+        "vectorizer_model_name": "Gurmukhi_vectorizer",
+        "vectorizer_type": "CountVectorizer",
+        "vectorizer_params": {
+            "ngram_range": "(3, 3)",
+            "max_features": 2000,
+            "analyzer": "char"
+        },
+        "vectorizer_downloaded": False,
+        "accuracy": "99.89%",
+    },
+    {
+        "script_name": "Hebrew",
+        "model_name": "Hebrew_model",
+        "downloaded": False,
+        "model_type": "Multinomial Naive Bayes",
+        "vectorizer_model_name": "Hebrew_vectorizer",
+        "vectorizer_type": "CountVectorizer",
+        "vectorizer_params": {
+            "ngram_range": "(3, 3)",
+            "max_features": 2000,
+            "analyzer": "char"
+        },
+        "vectorizer_downloaded": False,
+        "accuracy": "100%",
+    },
+    {
+        "script_name": "Latin",
+        "model_name": "Latin_model",
+        "downloaded": False,
+        "model_type": "Multinomial Naive Bayes",
+        "vectorizer_model_name": "Latin_vectorizer",
+        "vectorizer_type": "CountVectorizer",
+        "vectorizer_params": {
+            "ngram_range": "(3, 3)",
+            "max_features": 2000,
+            "analyzer": "char"
+        },
+        "vectorizer_downloaded": False,
+        "accuracy": "98.15%",
+    },
+    {
+        "script_name": "Malayalam",
+        "model_name": "Malayalam_model",
+        "downloaded": False,
+        "model_type": "Multinomial Naive Bayes",
+        "vectorizer_model_name": "Malayalam_vectorizer",
+        "vectorizer_type": "CountVectorizer",
+        "vectorizer_params": {
+            "ngram_range": "(3, 3)",
+            "max_features": 2000,
+            "analyzer": "char"
+        },
+        "vectorizer_downloaded": False,
+        "accuracy": "99.59%",
+    },
+    {
+        "script_name": "Myanmar",
+        "model_name": "Myanmar_model",
+        "downloaded": False,
+        "model_type": "Multinomial Naive Bayes",
+        "vectorizer_model_name": "Myanmar_vectorizer",
+        "vectorizer_type": "CountVectorizer",
+        "vectorizer_params": {
+            "ngram_range": "(3, 3)",
+            "max_features": 2000,
+            "analyzer": "char"
+        },
+        "vectorizer_downloaded": False,
+        "accuracy": "99.96%",
+    },
+    {
+        "script_name": "Oriya",
+        "model_name": "Oriya_model",
+        "downloaded": False,
+        "model_type": "Multinomial Naive Bayes",
+        "vectorizer_model_name": "Oriya_vectorizer",
+        "vectorizer_type": "CountVectorizer",
+        "vectorizer_params": {
+            "ngram_range": "(3, 3)",
+            "max_features": 2000,
+            "analyzer": "char"
+        },
+        "vectorizer_downloaded": False,
+        "accuracy": "99.94%",
+    },
+    {
+        "script_name": "Tamil",
+        "model_name": "Tamil_model",
+        "downloaded": False,
+        "model_type": "Multinomial Naive Bayes",
+        "vectorizer_model_name": "Tamil_vectorizer",
+        "vectorizer_type": "CountVectorizer",
+        "vectorizer_params": {
+            "ngram_range": "(3, 3)",
+            "max_features": 2000,
+            "analyzer": "char"
+        },
+        "vectorizer_downloaded": False,
+        "accuracy": "99.94%",
+    },
+    {
+        "script_name": "Telugu",
+        "model_name": "Telugu_model",
+        "downloaded": False,
+        "model_type": "Multinomial Naive Bayes",
+        "vectorizer_model_name": "Telugu_vectorizer",
+        "vectorizer_type": "CountVectorizer",
+        "vectorizer_params": {
+            "ngram_range": "(3, 3)",
+            "max_features": 2000,
+            "analyzer": "char"
+        },
+        "vectorizer_downloaded": False,
+        "accuracy": "99.94%",
+    },
+    {
+        "script_name": "Thai",
+        "model_name": "Thai_model",
+        "downloaded": False,
+        "model_type": "Multinomial Naive Bayes",
+        "vectorizer_model_name": "Thai_vectorizer",
+        "vectorizer_type": "CountVectorizer",
+        "vectorizer_params": {
+            "ngram_range": "(3, 3)",
+            "max_features": 2000,
+            "analyzer": "char"
+        },
+        "vectorizer_downloaded": False,
+        "accuracy": "100%",
+    },
+]
