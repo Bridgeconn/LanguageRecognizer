5ebdaf5b23c86e7ebf7bc76f2ec46e992ce78fc8
glad.e <gladysann1307@gmail.com>
2024-11-28 16:46:49 +0530

    Updated code to handle unwanted folder creation for unavailable models

--- src/language_detect/langrecognizer.py
+++ src/language_detect/langrecognizer.py
@@ -1,8 +1,9 @@
 '''Take input text and output its language and script'''
 from huggingface_hub.utils import enable_progress_bars
 from huggingface_hub import hf_hub_download
-from .script_detector import detect_script
-from .ld_dir import create_ld_dir
+from script_detector import detect_script
+from ld_dir import create_ld_dir
+import shutil
 import joblib
 import os
 
@@ -17,9 +18,6 @@ def recognize_language(text):
     try:
         script_name = detect_script(text)
 
-        if not script_name:
-            raise ValueError("Script detection failed.")
-
         home_dir = os.path.expanduser("~")
         models_dir = os.path.join(home_dir, ".ld_data", "models")
 
@@ -31,23 +29,35 @@ def recognize_language(text):
         vectorizer_path = os.path.join(models_dir, f"{script_name}-vectorizer", vectorizer_filename)
         model_path = os.path.join(models_dir, f"{script_name}-model", model_filename)
 
-        if not os.path.exists(vectorizer_path):
-            print(f"{vectorizer_filename} not found locally. Downloading...")
-            filename=f"{script_name}-vectorizer/{vectorizer_filename}"
+        try:
             vectorizer_path = hf_hub_download(
-            repo_id=repo_id,
-            filename=filename,
-            local_dir=models_dir,
+                repo_id=repo_id,
+                filename=f"{script_name}-vectorizer/{vectorizer_filename}",
+                local_dir=models_dir,
             )
-
-        if not os.path.exists(model_path):
-            print(f"{model_filename} not found locally. Downloading...")
-            filename = f"{script_name}-model/{model_filename}"
+        
+        except FileNotFoundError as e:
+            raise FileNotFoundError(f"Error: Vectorizer not found for {script_name}") from e
+        
+        finally:
+            partial_dir = os.path.join(models_dir, f"{script_name}-vectorizer")
+            if os.path.exists(partial_dir):
+                shutil.rmtree(partial_dir)
+            
+        try:
             model_path = hf_hub_download(
-            repo_id=repo_id,
-            filename=filename,
-            local_dir=models_dir,
+                repo_id=repo_id,
+                filename=f"{script_name}-model/{model_filename}",
+                local_dir=models_dir,
             )
+        except FileNotFoundError as e:
+            raise FileNotFoundError(f"Error: Model not found for {script_name}") from e
+            
+        finally:  
+            partial_dir = os.path.join(models_dir, f"{script_name}-model")
+            if os.path.exists(partial_dir):
+                shutil.rmtree(partial_dir)
+            
 
         vectorizer = joblib.load(vectorizer_path)
         vectorized_text = vectorizer.transform([text])
@@ -56,7 +66,7 @@ def recognize_language(text):
         language_name = str(model.predict(vectorized_text)[0])
 
         return language_name, script_name
-    except FileNotFoundError as fnf_error:
-        print(f"Error: {fnf_error}")
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"File error: {e}") from e
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        raise RuntimeError(f"An unexpected error occurred: {e}") from e
